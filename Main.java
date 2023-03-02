2d78b77d09ee09c34afe05251b2bcfd0f808b4b2
LDCS-015 <luisdavidcamacho15@gmail.com>
2023-03-02 10:34:33 -0500

    Changed to Specific Texts

--- Main.java
+++ Main.java
@@ -9,6 +9,45 @@ class Main {
 		ArrayList<String> arrayList = new ArrayList<>();
 		FileReader fr = new FileReader("names.txt");
 		Scanner fileScanner = new Scanner(fr);
+		Scanner sc = new Scanner(System.in);
+		while(fileScanner.hasNextLine()){
+			arrayList.add(fileScanner.nextLine());
+		}
+
+
+		int check = 0;
+		while(check != 5)
+		{
+			System.out.println("Press 1 to learn about salary.");
+			System.out.println("Press 2 to learn about the job.");
+			System.out.println("Press 3 to learn about demand");
+			System.out.println("Press 4 to view current students	.");
+			System.out.println("Press 5 to quit.");
+
+			check = sc.nextInt();
+			switch(check){
+				case 1:
+				System.out.println("$98,345 average salary in South Florida!");
+				break;
+				case 2:
+				System.out.println("US News - 100 Best Jobs!");
+				break;
+				case 3:
+				System.out.println("Top 10 Forbes In-Demand Jobs!");
+				break;
+				case 4:
+				System.out.println("Current Students:");
+				for(String i : arrayList)
+					System.out.println(i);
+				//System.out.println("\n");
+				break;
+				default:
+				//System.out.println("Have a nice day");
+			}
+
+					
+		}
+
 		
 	}
 }
